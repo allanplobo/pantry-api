22017e1b44e2d94d9e0d89444785df936a0088a8
Allan Lobo <allanplobo@gmail.com>
2023-03-05 18:28:55 -0300

    tests: implement tests in app controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PantryModule } from './pantry/pantry.module';
 
-describe('AppController', () => {
-  let appController: AppController;
+describe('AppModule', () => {
+  let app: TestingModule;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017/pantry'),
+        PantryModule,
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
-
-    appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('should be defined', () => {
+    expect(app).toBeDefined();
+  });
+
+  it('should import MongooseModule', () => {
+    expect(app.get(MongooseModule)).toBeDefined();
+  });
+
+  it('should import PantryModule', () => {
+    expect(app.get(PantryModule)).toBeDefined();
+  });
+
+  it('should import AppController', () => {
+    expect(app.get(AppController)).toBeDefined();
+  });
+
+  it('should import AppService', () => {
+    expect(app.get(AppService)).toBeDefined();
   });
 });
